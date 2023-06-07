377326aa97cbeb2fbdba12030e5b8f7d7bf6afec
Han Zaw Nyein <hanzawnyein@Hans-MacBook-Pro-2.local>
2023-06-07 13:56:10 +0630

    [add] company sorting ASC by js template extend

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Company Sorting",
+    "author": "Han Zaw Nyein",
+    "depends": ["web"],
+    'assets': {
+        'web.assets_qweb': [
+            'company_sorting/static/src/xml/switch_company_menu.xml'
+        ],
+    }
+}
